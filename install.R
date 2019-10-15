1906531dfb4e9a539b991f9457b9a71d1e73237b
Felipe <lipecaso@gmail.com>
2019-10-15 14:59:34 -0300

    editing file install

--- install.R
+++ install.R
@@ -1,1 +1,12 @@
-install.packages(c("tidyverse", "scales", "lubridate", "kohonen"))
+install.packages("devtools")
+
+devtools::install_version(c("tidyverse", 
+                            "dplyr", 
+                            "kohonen", 
+                            "scales", 
+                            "lubridate"), 
+                          version = c("1.2.1", 
+                                      "0.8.0.1", 
+                                      "3.0.8", 
+                                      "1.0.0",
+                                      "1.7.4"))
